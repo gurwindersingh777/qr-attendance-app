b8adbe2c04533ba940fe11e2db60bf73c09b974d
Gurwinder Singh <singhgurwinder2004@gmail.com>
2026-05-09 21:42:52 +0530

    initialize client with Vite React, TailwindCSS, and TypeScript

---
+++ client/src/config/apiClient.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+})
+
+api.interceptors.response.use(
+  (response) => response,
+
+  async (error) => {
+    const original = error.config as any
+    const isAuthRoute = original?.url?.includes("/auth/")
+
+    if (error.response?.status === 401 && !original._retry && !isAuthRoute) {
+      original._retry = true;
+      try {
+        await api.get("/auth/refresh")
+        return api(original);
+      } catch {
+        window.location.href = "/login"
+      }
+    }
+
+    return Promise.reject(error)
+  }
+)
+
+export default api
